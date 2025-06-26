e956ca5d2dcdb9fd65fc342ab527229d69e29d68
louisgeek <louisgeek@qq.com>
2025-06-26 15:56:30 +0800

    mod

--- library/src/main/java/com/louisgeek/library/tool/ScreenTool.kt
+++ library/src/main/java/com/louisgeek/library/tool/ScreenTool.kt
@@ -1,5 +1,6 @@
 package com.louisgeek.library.tool
 
+import android.annotation.SuppressLint
 import android.content.res.Resources
 
 /**
@@ -12,4 +13,28 @@ object ScreenTool {
     //状态栏 + 标题栏 + 内容 + 导航栏
     @JvmStatic
     val screenHeight = Resources.getSystem().displayMetrics.heightPixels
+
+    @JvmStatic
+    @SuppressLint("InternalInsetResource")
+    fun getStatusBarHeight(): Int {
+        var statusBarHeight = 0
+        val resourceId =
+            Resources.getSystem().getIdentifier("status_bar_height", "dimen", "android")
+        if (resourceId > 0) {
+            statusBarHeight = Resources.getSystem().getDimensionPixelSize(resourceId)
+        }
+        return statusBarHeight
+    }
+
+    @JvmStatic
+    @SuppressLint("InternalInsetResource")
+    fun getNavigationBarHeight(): Int {
+        var navigationBarHeight = 0
+        val resourceId =
+            Resources.getSystem().getIdentifier("navigation_bar_height", "dimen", "android")
+        if (resourceId > 0) {
+            navigationBarHeight = Resources.getSystem().getDimensionPixelSize(resourceId)
+        }
+        return navigationBarHeight
+    }
 }
